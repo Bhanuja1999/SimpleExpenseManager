06dc6c33a309501c593424886c2f97b2a9341aee
Bhanuja1999 <90821129+Bhanuja1999@users.noreply.github.com>
2022-05-04 09:22:12 +0530

    Final Changes

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -0,0 +1,78 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.DBContract;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+
+public class PersistentExpenseManager extends ExpenseManager {
+    public static final String DATABASE_NAME = "190152B";
+    public static final int DATABASE_VERSION = 1;
+    private static final String[] SQL_CREATE = {
+            String.format("CREATE TABLE %s(%s text PRIMARY KEY, %s text, %s text, %s real);",
+                    DBContract.Account.TABLE_NAME, DBContract.Account.COLUMN_NAME_ACCOUNT_NO,
+                    DBContract.Account.COLUMN_NAME_BANK_NAME,
+                    DBContract.Account.COLUMN_NAME_HOLDER_NAME,
+                    DBContract.Account.COLUMN_NAME_BALANCE),
+            String.format("CREATE TABLE %s(%s int PRIMARY KEY, %s timestamp, %s text, %s text, %s real, FOREIGN KEY (%s) REFERENCES %s(%s));",
+                    DBContract.Transaction.TABLE_NAME, DBContract.Transaction._ID,
+                    DBContract.Transaction.COLUMN_NAME_DATE, DBContract.Transaction.COLUMN_NAME_ACCOUNT_NO,
+                    DBContract.Transaction.COLUMN_NAME_EXPENSE_TYPE,
+                    DBContract.Transaction.COLUMN_NAME_AMOUNT,
+                    DBContract.Transaction.COLUMN_NAME_ACCOUNT_NO, DBContract.Account.TABLE_NAME,
+                    DBContract.Account.COLUMN_NAME_ACCOUNT_NO),
+    };
+    private static final String[] SQL_DELETE = {
+            "DROP TABLE IF EXISTS " + DBContract.Account.TABLE_NAME,
+            "DROP TABLE IF EXISTS " + DBContract.Transaction.TABLE_NAME};
+    private final DatabaseHelper helper;
+
+    public PersistentExpenseManager(Context context) {
+        helper = new DatabaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
+        setup();
+    }
+
+    @Override
+    public void setup() {
+        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO(helper);
+        setTransactionsDAO(persistentTransactionDAO);
+
+        AccountDAO persistentAccountDAO = new PersistentAccountDAO(helper);
+        setAccountsDAO(persistentAccountDAO);
+
+        // dummy data
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+    }
+
+    static class DatabaseHelper extends SQLiteOpenHelper {
+        public DatabaseHelper(Context context, String databaseName, CursorFactory cursorFactory,
+                              int databaseVersion) {
+            super(context, databaseName, cursorFactory, databaseVersion);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            for (String command : SQL_CREATE) {
+                db.execSQL(command);
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            for (String command : SQL_DELETE) {
+                db.execSQL(command);
+            }
+            onCreate(db);
+        }
+    }
+}
---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -0,0 +1,67 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.DBContract;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+
+public class PersistentTransactionDAO implements TransactionDAO {
+    private final SQLiteOpenHelper helper;
+
+    public PersistentTransactionDAO(SQLiteOpenHelper helper) {
+        this.helper = helper;
+    }
+
+    @Override
+    public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DBContract.Transaction.COLUMN_NAME_DATE, date.getTime());
+        values.put(DBContract.Transaction.COLUMN_NAME_ACCOUNT_NO, accountNo);
+        values.put(DBContract.Transaction.COLUMN_NAME_EXPENSE_TYPE, expenseType.name());
+        values.put(DBContract.Transaction.COLUMN_NAME_AMOUNT, amount);
+        db.insert(DBContract.Transaction.TABLE_NAME, null, values);
+    }
+
+    @Override
+    public List<Transaction> getAllTransactionLogs() {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.query(DBContract.Transaction.TABLE_NAME, null, null, null, null, null, null);
+        List<Transaction> transactions = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            long time = cursor.getLong(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_DATE));
+            String accountNo = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_ACCOUNT_NO));
+            String expenseType = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_EXPENSE_TYPE));
+            double amount = cursor.getDouble(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_AMOUNT));
+            transactions.add(new Transaction(new Date(time), accountNo,ExpenseType.valueOf(expenseType), amount));
+        }
+        cursor.close();
+        return transactions;
+    }
+
+    @Override
+    public List<Transaction> getPaginatedTransactionLogs(int limit) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.query(DBContract.Transaction.TABLE_NAME, null, null, null, null, null,
+                null, Integer.toString(limit));
+        List<Transaction> transactions = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            long time = cursor.getLong(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_DATE));
+            String accountNo = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_ACCOUNT_NO));
+            String expenseType = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_EXPENSE_TYPE));
+            double amount = cursor.getDouble(cursor.getColumnIndexOrThrow(DBContract.Transaction.COLUMN_NAME_AMOUNT));
+            transactions.add(new Transaction(new Date(time), accountNo, ExpenseType.valueOf(expenseType), amount));
+        }
+        cursor.close();
+        return transactions;
+    }
+}
